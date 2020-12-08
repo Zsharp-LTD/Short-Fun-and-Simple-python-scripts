cef487cfe76c59c647a51f46b95f1513fac1cfac
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-12-08 16:48:58 +1300

    First addition to this repository

---
+++ Give me your money! ( ͡☉ ͜ʖ ͡☉).py
@@ -0,0 +1,6 @@
+from turtle import *
+
+color('deep pink')
+style = ('Courier', 50, 'italic')
+write("Give me your money! (͡☉͜ʖ͡☉)", font=style, align='center')
+hideturtle()
---
+++ Spam the console.py
@@ -0,0 +1,9 @@
+#spam the console!
+
+Word = input("What would you like to spam the console with? ")
+
+while True:
+    print(Word)
+
+    
+
