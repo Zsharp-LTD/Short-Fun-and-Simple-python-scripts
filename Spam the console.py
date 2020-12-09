807e062a9cb85562be187619673deb08b00dbb43
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-12-10 09:56:59 +1300

    added basic comments

--- Spam the console.py
+++ Spam the console.py
@@ -1,9 +1,9 @@
 #spam the console!
 
+#define what we want to spam via user input
 Word = input("What would you like to spam the console with? ")
 
+#loop spamming
 while True:
     print(Word)
-
     
-
