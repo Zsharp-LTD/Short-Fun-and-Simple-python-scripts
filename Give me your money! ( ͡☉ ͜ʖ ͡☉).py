a8d2275e32dd81adaa37e650f288beb26f03cfe9
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-12-10 09:55:41 +1300

    added basic comments

--- Give me your money! ( ͡☉ ͜ʖ ͡☉).py
+++ Give me your money! ( ͡☉ ͜ʖ ͡☉).py
@@ -1,6 +1,12 @@
+#import function
 from turtle import *
 
+#set text variables
 color('deep pink')
 style = ('Courier', 50, 'italic')
+
+#write text 2 window
 write("Give me your money! (͡☉͜ʖ͡☉)", font=style, align='center')
 hideturtle()
+
+#wait for those patreon donations...
